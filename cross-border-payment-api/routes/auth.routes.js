33823260ee3ffa4a9e865286d0ed96f2da6fa2fa
LAMXINLE <xlam0003@student.monash.edu>
2023-01-15 14:36:15 +0800

    add routes for authentication

---
+++ cross-border-payment-api/routes/auth.routes.js
@@ -0,0 +1,24 @@
+const { verifySignUp } = require("../middlewares");
+const controller = require("../controllers/auth.controller");
+
+const BASE_URL = "/api/auth";
+
+module.exports = function (app) {
+  app.use(function (req, res, next) {
+    res.header(
+      "Access-Control-Allow-Headers",
+      "x-access-token, Origin, Content-Type, Accept"
+    );
+    next();
+  });
+
+  app.post(
+    BASE_URL + "/signup",
+    [
+      verifySignUp.checkDuplicateUsernameOrEmail
+    ],
+    controller.signup
+  );
+
+  app.post(BASE_URL + "/signin", controller.signin);
+};
