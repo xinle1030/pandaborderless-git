ce006308d2342200ed2964369c8a3a320e4c62eb
LAMXINLE <xlam0003@student.monash.edu>
2023-01-15 12:59:26 +0800

    add TransactionHistory Model

---
+++ cross-border-payment-api/models/TransactionHistory.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const transactionHistorySchema = new mongoose.Schema({
+  transactionId: {
+    type: Number,
+    required: true,
+  },
+  // The user associated with the transaction
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Customer",
+  },
+  accountFrom: {
+    type: Number,
+    required: true,
+  },
+  accountTo: {
+    type: Number,
+    required: true,
+  },
+  transactionHash: {
+    type: String,
+    required: true,
+  },
+  transactionAmount: {
+    type: Number,
+    required: true,
+  },
+  dateTime: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports =
+  mongoose.models.TransactionHistory ||
+  mongoose.model("TransactionHistory", transactionHistorySchema);
