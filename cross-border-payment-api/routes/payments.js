31ca8b7a12bcb95dc14208b3fd03530b5f6cc6c1
LAMXINLE <xlam0003@student.monash.edu>
2023-01-15 15:24:01 +0800

    update code outline for transfer under endpoint /api/payments/crossborder/confirm

--- cross-border-payment-api/routes/payments.js
+++ cross-border-payment-api/routes/payments.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config/.env" });
+const Account = require("../models/Account");
 
 const BASE_URL = "/api/payments/crossborder";
 
@@ -9,8 +10,59 @@ module.exports = function (app, lms, accounts) {
     res.send("Welcome to cross border payments");
   });
 
-  app.get(BASE_URL + "/confirm", (req, res) => {
-    res.send("Confirm multiple previously initiated cross-border payments");
+  app.put(BASE_URL + "/confirm", async (req, res) => {
+    // Transfer a value between accounts informed
+
+    /**
+     * AccountTo, AccountFrom, TransferAmount
+     */
+
+    const amountToTransfer = req.body.amountToTransfer;
+
+    let accountFromPK;
+    let accountFromWalletAddr;
+    let accountToWalletAddr;
+
+    /**
+     * Future TODO - a separate function
+     *
+     * currency exchange
+     * check accountTo currency and accountFrom currency
+     * if different currency, convert to accountTo currency using an external exchange rate API
+     */
+
+    /**
+     * Future TODO - 1-1 peg currency to pandacoin
+     *
+     */
+
+    /**
+     * To do
+     */
+    await Account.findOne({
+      accountNumber: req.body.accountFrom,
+    }).exec((err, accountFrom) => {
+      if (err) {
+        res.status(500).send({ message: err });
+        return;
+      }
+      accountFromPK = accountFrom.walletPKHash;
+      accountFromWalletAddr = accountFrom.walletAdrHash;
+    });
+
+    await Account.findOne({
+      accountNumber: req.body.accountTo,
+    }).exec((err, accountTo) => {
+      if (err) {
+        res.status(500).send({ message: err });
+        return;
+      }
+      accountToWalletAddr = accountTo.walletAdrHash;
+    });
+
+    
+
+    // Expected response: transaction history object
   });
 
   app.get(BASE_URL + "/:paymentId", (req, res) => {
