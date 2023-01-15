4775a31dd401e2bcdbf036ecef8a3d3670f1d4df
LAMXINLE <xlam0003@student.monash.edu>
2023-01-15 09:15:41 +0800

    deploy contract

--- cross-border-payment-api/migrations/2_deploy_contracts.js
+++ cross-border-payment-api/migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 const SimpleStorage = artifacts.require("./SimpleStorage.sol");
+const PandaCoin = artifacts.require("./PandaCoin.sol");
 
 module.exports = function (deployer) {
   deployer.deploy(SimpleStorage, 1000);
+  deployer.deploy(PandaCoin);
 };
