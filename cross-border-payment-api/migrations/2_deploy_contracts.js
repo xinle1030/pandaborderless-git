6faee57f9b6b210ad770eddff30de9af7e4dd04f
LAMXINLE <xlam0003@student.monash.edu>
2023-01-16 16:08:09 +0800

    add in deployment of currencyexchange smart contract

--- cross-border-payment-api/migrations/2_deploy_contracts.js
+++ cross-border-payment-api/migrations/2_deploy_contracts.js
@@ -1,7 +1,9 @@
 const SimpleStorage = artifacts.require("./SimpleStorage.sol");
 const PandaCoin = artifacts.require("./PandaCoin.sol");
+const CurrencyExchange = artifacts.require("./CurrencyExchange.sol");
 
 module.exports = function (deployer) {
   deployer.deploy(SimpleStorage, 1000);
   deployer.deploy(PandaCoin);
+  deployer.deploy(CurrencyExchange);
 };
