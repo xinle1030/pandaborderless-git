877611d35eff00d37cf70235028396a1b752c8db
LAMXINLE <xlam0003@student.monash.edu>
2023-01-16 16:49:04 +0800

    remove unused package

--- cross-border-payment-api/routes/index.js
+++ cross-border-payment-api/routes/index.js
@@ -1,11 +1,10 @@
-const express = require("express");
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config/.env" });
 
 const BASE_URL = "/api/payments/crossborder";
 
-module.exports = function (app, lms, accounts) {
+module.exports = function (app) {
   app.get("/", (req, res) => {
-    res.send("Welcome to cross border payments");
+    res.send("Welcome to cross border payment platform");
   });
 };
