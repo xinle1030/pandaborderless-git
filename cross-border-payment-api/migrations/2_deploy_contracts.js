450809d08fae90a6852024bdcaf512f9e7f189c7
LAMXINLE <xlam0003@student.monash.edu>
2023-01-23 22:05:07 +0800

    remove deployment of unused contract

--- cross-border-payment-api/migrations/2_deploy_contracts.js
+++ cross-border-payment-api/migrations/2_deploy_contracts.js
@@ -1,9 +1,5 @@
-const SimpleStorage = artifacts.require("./SimpleStorage.sol");
 const PandaCoin = artifacts.require("./PandaCoin.sol");
-const CurrencyExchange = artifacts.require("./CurrencyExchange.sol");
 
 module.exports = function (deployer) {
-  deployer.deploy(SimpleStorage, 1000);
   deployer.deploy(PandaCoin);
-  deployer.deploy(CurrencyExchange);
 };
