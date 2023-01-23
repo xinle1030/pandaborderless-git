ca19ef035f1305c2cbc96658ec14bac2e39c4581
LAMXINLE <xlam0003@student.monash.edu>
2023-01-23 21:36:39 +0800

    refactor transfer acc path by adding mongoose commit transaction

--- cross-border-payment-api/controllers/account/updateAccount.js
+++ cross-border-payment-api/controllers/account/updateAccount.js
@@ -1,75 +1,98 @@
-const Account = require("../../models/Account");
-const Customer = require("../../models/Customer");
-const ethereumjs = require("ethereumjs-wallet");
-const Web3 = require("web3");
 const dotenv = require("dotenv");
 dotenv.config({ path: "../../config/.env" });
 const accUtils = require("../../utils/acc_utils");
 const fxUtils = require("../../utils/fx_utils");
+const txnUtils = require("../../utils/txn_utils");
+const mongoose = require("mongoose");
 
 const PEG_CURRENCY = process.env.PEG_CURRENCY;
 
+module.exports = async (req, res, lms, web3) => {
+  let { accountFrom, accountTo, amountToTransfer } = req.body;
 
+  const owner = await accUtils.getPDCOwner();
+  const sender = await accUtils.retrieveDocumentByAccountNumber(accountFrom);
+  const receiver = await accUtils.retrieveDocumentByAccountNumber(accountTo);
 
-const ownerToSender = async (owner, senderAddr, amount) => {
-  let ownerAddr = owner.ownerWallet;
-  let ownerPK = owner.ownerPK;
+  if (amountToTransfer > sender.balance) {
+    const error = "Sender Account Balance not sufficient!";
+    res.status(400).send({ message: error });
+    throw new Error(error);
+  }
 
-
-};
-
-const makeTransaction = async (sender, receiver, amount, lms) => {
   const fromCurrency = sender.currency;
   const toCurrency = receiver.currency;
-
-  const fromAddr = sender.walletAdrHash;
-  const toAddr = receiver.walletAdrHash;
 
   let convertAmount = await fxUtils.fxConvert(
     fromCurrency,
     PEG_CURRENCY,
-    amount
+    amountToTransfer
   );
   let convertAmountInPDC = await fxUtils.SGDtoPDC(convertAmount);
 
-  console.log(convertAmountInPDC);
+  let receivedAmountInSGD = await fxUtils.PDCtoSGD(convertAmountInPDC);
+  let receivedAmount = await fxUtils.fxConvert(
+    PEG_CURRENCY,
+    toCurrency,
+    receivedAmountInSGD
+  );
 
-  const owner = await accUtils.getPDCOwner();
+  const session = await mongoose.startSession();
+  session.startTransaction();
 
-  // // Update the exchange rate
-  // FX_LMS.methods
-  //   .updateExchangeRate(fromCurrency, toCurrency, 3)
-  //   .send({ from: fromAddr }, (error, transactionHash) => {
-  //     if (error) {
-  //       console.log(error);
-  //       res.json({ success: false, error: error.message });
-  //     } else {
-  //       console.log(transactionHash);
-  //     }
-  //   });
+  try {
+    // PANDA ACC to RECEIVER
+    const hash1 = await txnUtils.makeTransaction(
+      owner,
+      receiver,
+      convertAmountInPDC,
+      web3,
+      lms
+    );
 
-  // // Exchange currency
-  // FX_LMS.methods
-  //   .exchange(fromAddr, amount, fromCurrency, toCurrency)
-  //   .send({ from: fromAddr }, (error, transactionHash) => {
-  //     if (error) {
-  //       console.log(error);
-  //       res.json({ success: false, error: error.message });
-  //     } else {
-  //       res.json({ success: true, transactionHash });
-  //       console.log(transactionHash);
-  //     }
-  //   });
-};
+    // RECEIVER to PANDA ACC
+    const hash2 = await txnUtils.makeTransaction(
+      receiver,
+      owner,
+      convertAmountInPDC,
+      web3,
+      lms
+    );
 
-module.exports = async (req, res, lms) => {
-  let { accountFrom, accountTo, amountToTransfer } = req.body;
+    console.log(hash1);
+    console.log(hash2);
 
-  const sender = await accUtils.retrieveDocumentByAccountNumber(accountFrom);
-  const receiver = await accUtils.retrieveDocumentByAccountNumber(accountTo);
+    if (hash1 && hash2) {
+      try {
+        await accUtils.deductBalance(accountFrom, amountToTransfer);
+        await accUtils.increaseBalance(accountTo, receivedAmount);
+        await txnUtils.createTxnHistory(
+          sender,
+          receiver,
+          amountToTransfer,
+          hash1
+        );
 
-  makeTransaction(sender, receiver, amountToTransfer, lms);
-  // res.send(fxConvert(fromCurrency, toCurrency, amountToTransfer));
+        await session.commitTransaction();
+        session.endSession();
 
-  // exchangeRate(sender, receiver, amountToTransfer, FX_LMS, res);
+        // Expected response: transaction history object
+        res.send("Transaction hash completed with: " + hash1 + ", " + hash2 + " from " + accountFrom + " to " + accountTo);
+      } catch (err) {
+        await session.abortTransaction();
+        session.endSession();
+        console.log(err);
+        return res.status(500).json("Bank account update and transaction fail");
+      }
+    } else {
+      await session.abortTransaction();
+      session.endSession();
+      return res.status(500).json("Wallet transaction fail");
+    }
+  } catch (err) {
+    await session.abortTransaction();
+    session.endSession();
+    console.log(err);
+    return res.status(500).json("Server Error");
+  }
 };
